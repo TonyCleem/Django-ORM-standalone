d1df4122fbbab9eb48d5b3757d74ab7a7784d8d1
TonyCleem <tonycleem@gmail.com>
2024-12-12 23:05:59 +0700

    end step 12

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import django
 import datetime
 from django.utils.timezone import localtime
+from timer import get_duration, format_duration
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -24,10 +26,36 @@ if __name__ == '__main__':
     #     print(stay_time)
 
     #шаг 11
-    employees_from_storage = Visit.objects.filter(leaved_at__isnull=True)
-    for employee in employees_from_storage:
-        employee_name = employee.passcard.owner_name
-        print(employee_name)
+    # visits = Visit.objects.filter(leaved_at__isnull=True)
+    # for visit in visits:
+    #     employee_name = visit.passcard.owner_name
+    #     entered_at = visit.entered_at
+    #     string_entered_at = entered_at.strftime("%m/%d/%Y, %H:%M:%S")
+    #     duration = get_duration(visit)
+    #     duration = format_duration(duration)
+    #     print(duration)
+
+    # # Шаг 12
+    # visits = Visit.objects.filter(leaved_at__isnull=True)
+    # for visit in visits:
+    #     employee_name = str(visit.passcard.owner_name)
+    #     entered_at = visit.entered_at
+    #     duration = get_duration(visit)
+    #     duration = format_duration(duration)
+
+    #     months = {
+    #             1: "января", 2: "февраля", 3: "марта", 4: "апреля", 5: "мая", 6: "июня",
+    #             7: "июля", 8: "августа", 9: "сентября", 10: "октября", 11: "ноября", 12: "декабря"
+    #         }
+
+    #     entered_at = f"{entered_at.day} {months[entered_at.month]} {entered_at.year} г. {entered_at.strftime('%H:%M')}"
+
+
+        # Шаг 13
+
+
+
+
 
    
 
